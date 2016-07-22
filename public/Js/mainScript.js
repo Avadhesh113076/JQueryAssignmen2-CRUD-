ef526d3d7df427227630a204a0428d6cef3730a6
Avadhesh <avadhesh.1001@gmail.com>
2016-07-22 18:48:47 +0530

    JqueryAssignment2

--- public/Js/mainScript.js
+++ public/Js/mainScript.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 			   success: function(data) {
 
 				   	   for(var i=0;i<data.length;i++){
-				   	   	    console.log(data.length);
+				   	   	    
 				   	   	 
 				   	   	  var addRow = '<tr><td>'+data[i].id+'</td><td>'
 				   	   	  					 +data[i].bookName+'</td><td>'
@@ -25,11 +25,14 @@ $(document).ready(function(){
 				   	   	  					 +data[i].email+'</td><td>'
 				   	   	  					 +data[i].phone+'</td><td>'
 				   	   	  					 +data[i].published+
-				   	   	  					 '</td><td><button class="btn btn-info" type="button" id="update">Update</button><button class="btn btn-danger" type="button">Delete</button></td></tr>'
+				   	   	  					 '</td><td>'+
+				   	   	  					 '<button class="btn btn-info" type="button"'+ 
+				   	   	  					   'id="update'+i+'">Update</button>'+
+				   	   	  					 '<button class="btn btn-danger"' +
+				   	   	  					 'type="button" id="delete">Delete</button></td></tr>'
 				   	   	  					 ;
-
-				   	   	  
-				   	   	  				 
+				   	   	  			 
+				   	   	  	 
 				   	   		$('#info').append(addRow);
 				   	   }
 
@@ -43,40 +46,40 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("button#add").click(function(){
     	var inputBookName=$("#inputBookName").val(),
-			   				    inputPrice=$("#inputPrice").val(),
-			   				    inputAuthor=$("#inputAuthor").val(),
-			   				    inputEmail=$("#inputEmail").val(),
-			   				    inputPhone=$("#inputPhone").val(),
-			   				    inputPublished=$("#inputPublished").val();
+			inputPrice=$("#inputPrice").val(),
+			inputAuthor=$("#inputAuthor").val(),
+			inputEmail=$("#inputEmail").val(),
+			inputPhone=$("#inputPhone").val(),
+			inputPublished=$("#inputPublished").val();
 
-			   				    var sendInfo={
-			   				    	
-    											 bookName: inputBookName,
-    											 price: inputPrice,
-    											 publisherName: inputAuthor,
-    											 email: inputEmail,
-    											 phone: inputPhone,
-    											 published: inputPublished
-    										
-			   				    }
+
         $.ajax({
 				type:"Post",
 			   url: ' http://localhost:8080/book',
-			   
-			   error: function() {
-			      $('#info').html('<p>An error has occurred</p>');
-			   },
-			   
-			   success: function(data) {
-			   				
-									data:sendInfo;		
-				   	   	  console.log(data);
-			      }
+			   headers:{'Content-Type':'application/json'},
+               
+			   data: JSON.stringify({
+			   	        bookName: inputBookName,				 
+    					price: inputPrice,
+   						publisherName: inputAuthor,
+    					email: inputEmail,
+    				    phone: inputPhone,
+    					published: inputPublished
+			   })
 			   
 			});
-           
+           console.log("Added");
     });
 });
+
+$(document).ready(function(){
+
+    	$('table#info td button#delete').click(function(){
+				   	   	  			console.log('delete');
+				   	   	  	});
+});
+
+
 
 
 
